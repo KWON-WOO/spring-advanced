bd7b77f420afbdc8b027ac18aa00af6dd1228306
KWON-WOO <ckw075@naver.com>
2025-12-03 16:26:52 +0900

    ♻ refactor: yml 세팅.
    프로젝트가 실행되도록 resource 경로와 appication.yml 파일 추가.

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+//@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ExpertApplication {
 
     public static void main(String[] args) {
